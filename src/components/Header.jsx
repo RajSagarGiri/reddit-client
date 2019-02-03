420925e8defa836257562294049ec4138dad7acd
Raj Sagar <therajsagar@gmail.com>
2019-02-04 00:42:15 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import '../styles/header.css';
 
-const Header =  () => 
-   <div className='header'>
-   <div className='img'>
+const Header =  (props) => 
+   <div className='head'>
     <img src='header.png' alt='Reddit' />
-    </div>
-    <div className='tab'>
-    < a href='#null'>ALL</a>
-    < a href='#null'>POPULAR</a>
-    < a href='#null'>RANDOM</a>
-    </div>
-    <hr/>
+    <form onSubmit={props.action}>
+        <input type='text'/>
+    </form>
     </div>
 export default Header;
 
