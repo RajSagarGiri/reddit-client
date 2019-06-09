480c20ac625df07b5364468d99e6236706a342c4
Raj Sagar <therajsagar@gmail.com>
2019-06-09 14:39:54 +0530

    Updates

---
+++ precache-manifest.afb7afb2af5db9fae734ce7fe12a7414.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "60fc21d422f6201d3676",
+    "url": "/reddit-client/static/css/main.3b9a4ca4.chunk.css"
+  },
+  {
+    "revision": "60fc21d422f6201d3676",
+    "url": "/reddit-client/static/js/main.60fc21d4.chunk.js"
+  },
+  {
+    "revision": "588d3689936bd3293666",
+    "url": "/reddit-client/static/js/1.588d3689.chunk.js"
+  },
+  {
+    "revision": "4a1ab66415effc6ba206",
+    "url": "/reddit-client/static/js/runtime~main.4a1ab664.js"
+  },
+  {
+    "revision": "9335e085dcc0573cadb7712b41e0b6f9",
+    "url": "/reddit-client/static/media/up.9335e085.png"
+  },
+  {
+    "revision": "0b07ad7bbc718676148b48f83176440c",
+    "url": "/reddit-client/static/media/bug.0b07ad7b.gif"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/reddit-client/static/media/spinner.0d0345f6.gif"
+  },
+  {
+    "revision": "1533a55beab7dbce682de3fbfcc326dd",
+    "url": "/reddit-client/index.html"
+  }
+];
