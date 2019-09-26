928f31ef46d7df9d6bdb2bc2e667f342c673993b
Raj Sagar <therajsagar@gmail.com>
2019-09-26 10:55:35 +0530

    Updates

---
+++ precache-manifest.5645004959185ff20bce42aa4a7954a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9199c116060a1f10f306c2c33046242",
+    "url": "/reddit-client/index.html"
+  },
+  {
+    "revision": "da2771cf61f0928ae836",
+    "url": "/reddit-client/static/css/main.5f42d8ed.chunk.css"
+  },
+  {
+    "revision": "18104aff040af349f563",
+    "url": "/reddit-client/static/js/2.0bc73698.chunk.js"
+  },
+  {
+    "revision": "da2771cf61f0928ae836",
+    "url": "/reddit-client/static/js/main.3465457d.chunk.js"
+  },
+  {
+    "revision": "63dae2cbe9661ccde0d8",
+    "url": "/reddit-client/static/js/runtime-main.2b54796e.js"
+  },
+  {
+    "revision": "0b07ad7bbc718676148b48f83176440c",
+    "url": "/reddit-client/static/media/bug.0b07ad7b.gif"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/reddit-client/static/media/spinner.0d0345f6.gif"
+  },
+  {
+    "revision": "9335e085dcc0573cadb7712b41e0b6f9",
+    "url": "/reddit-client/static/media/up.9335e085.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reddit-client/precache-manifest.a313eedad295e74a643391e0ef747bd4.js"
+  "/reddit-client/precache-manifest.5645004959185ff20bce42aa4a7954a8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reddit-client/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reddit-client/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
